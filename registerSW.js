6b1f51f1d1e1c8a0f288490d2efe2423fa75f6c4
konapuramsureshbabu <114557970+konapuramsureshbabu@users.noreply.github.com>
2025-04-10 11:23:35 +0000

    Deploying to gh-pages from @ konapuramsureshbabu/face-detection-app-fe@58810d582f45a4eff63d1d4f7d8fd777356c0da3 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/face-detection-app-fe/sw.js', { scope: '/face-detection-app-fe/' })})}
