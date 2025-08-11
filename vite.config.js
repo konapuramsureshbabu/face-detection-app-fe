7552f5d2e61eb4e0ac7fb7fbf0523b7d58732a3f
Sureshbabu Konapuram <114557970+konapuramsureshbabu@users.noreply.github.com>
2025-08-11 16:09:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react()],
 });
